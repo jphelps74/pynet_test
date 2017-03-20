56baccefdef36a977d3cf2d2bbc084adcd9fa0fa
Jarrod Phelps <jarrod@uber.com>
2017-03-20 14:13:45 -0700

    Enter Name

---
+++ ex23.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+name1 = 'John Doe'
+name2 = 'Mary Ann'
+name3 = 'Jane Adam'
+name4 = raw_input('Enter Name: ')
+print "{:>30} {:>30} {:>30} {:>30}".format(name1, name2, name3, name4)
