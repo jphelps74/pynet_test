58028c723a10a038a00d81c1a14e57f6521f996e
Jarrod Phelps <jarrod@uber.com>
2017-03-20 14:19:58 -0700

    Removed Spaces

--- ex2.py
+++ ex2.py
@@ -2,4 +2,4 @@
 name1 = 'John Doe'
 name2 = 'Mary Ann'
 name3 = 'Jane Adam'
-print "{:>30} {:>30} {:>30}".format(name1, name2, name3)
+print "{:>30}{:>30}{:>30}".format(name1, name2, name3)
--- ex23.py
+++ ex23.py
@@ -3,4 +3,4 @@ name1 = 'John Doe'
 name2 = 'Mary Ann'
 name3 = 'Jane Adam'
 name4 = raw_input('Enter Name: ')
-print "{:>30} {:>30} {:>30} {:>30}".format(name1, name2, name3, name4)
+print "{:>30}{:>30}{:>30}{:>30}".format(name1, name2, name3, name4)
