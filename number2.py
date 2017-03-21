b1b0770e22626c08482d0696ab2e12ed392ec1ad
Jarrod Phelps <jarrod@uber.com>
2017-03-20 22:47:40 -0700

    number2

---
+++ number2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+num_1 = int(raw_input('Enter Number1: '))
+num_2 = int(raw_input('Enter Number2: '))
+print "Sum {}".format(num_1 + num_2)
+print "Dif {}".format(num_1 - num_2)
+print "Product {}".format(num_1 * num_2)
+print "Div {}".format(num_1 / float(num_2))
+
+
