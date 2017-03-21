d323c8a0f52b02841b83560d062f7b9ee4849bb2
Jarrod Phelps <jarrod@uber.com>
2017-03-20 22:39:22 -0700

    numbers

---
+++ number1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+num_1 = raw_input('Enter Number: ')
+num_2 = raw_input('Enter Number: ')
+print int(num_1) + int(num_2)
+print int(num_1) - int(num_2)
+print int(num_1) * int(num_2)
+print int(num_1) / float(int(num_2))
+
+
